ab844eb917527c1197f3177b7f1cf31a0db48265
Farid Sharif Moghadam <faridmoghaddam.dev@gmail.com>
2025-08-27 23:56:37 +0330

    Add navigation drawer to MainActivity

--- app/src/main/java/com/faridev/gameradar/presentation/activity/main/MainActivity.kt
+++ app/src/main/java/com/faridev/gameradar/presentation/activity/main/MainActivity.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.faridev.gameradar.R
+import com.faridev.gameradar.core.util.showShortToast
 import com.faridev.gameradar.presentation.common.theme.GameRadarTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +45,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             GameRadarTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainNavigationDrawer(
+                        modifier = Modifier.padding(innerPadding),
+                        drawerItemsList = listOf(
+                            "Platforms",
+                            "Publishers",
+                            "Developers",
+                            "Genres",
+                            "Stores"
+                        )
                     )
                 }
             }
@@ -31,17 +62,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun MainNavigationDrawer(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
+    drawerItemsList: List<String>
+) {
+    ModalNavigationDrawer(
+        modifier = modifier,
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier.width(280.dp),
+                drawerContainerColor = MaterialTheme.colorScheme.secondaryContainer
+            ) {
+                MainNavigationDrawerContent(drawerItemsList)
+            }
+        }
+    ) {
+        MainScreenContent(navController = navController)
+    }
+}
+
+@Composable
+private fun MainNavigationDrawerContent(
+    drawerItemsList: List<String>
+) {
+    val context = LocalContext.current
+    Column {
+        Card(
+            modifier = Modifier.padding(10.dp),
+            shape = RoundedCornerShape(15.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2.2f),
+                painter = painterResource(R.drawable.banner),
+                contentScale = ContentScale.Crop,
+                contentDescription = "Banner"
+            )
+        }
+
+        LazyColumn(modifier = Modifier.padding(top = 10.dp)) {
+            items(drawerItemsList) { itemTitle ->
+                DrawerItem(itemTitle) {
+                    context.showShortToast(itemTitle)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun DrawerItem(itemTitle: String, onClick: () -> Unit = {}) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+        text = itemTitle,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleLarge
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GameRadarTheme {
-        Greeting("Android")
-    }
+private fun MainScreenContent(navController: NavHostController) {
+    //TODO implement later
 }
