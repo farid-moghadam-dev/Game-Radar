f6fbaf8fd0ad4c83a0a2f6f660216e1dd8ff3529
Farid Sharif Moghadam <faridmoghaddam.dev@gmail.com>
2025-08-28 20:51:37 +0330

    Feature/main activity UI schema (#2)
    * Add navigation drawer to MainActivity
    
    * Add home and detail navigation routes

---
+++ app/src/main/java/com/faridev/gameradar/presentation/feature/detail/GameDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.faridev.gameradar.presentation.feature.detail
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GameDetailScreen(
+    gameId: Int
+) {
+    Text("Game Detail Screen : $gameId")
+}
---
+++ app/src/main/java/com/faridev/gameradar/presentation/feature/detail/GameDetailViewModel.kt
@@ -0,0 +1,5 @@
+package com.faridev.gameradar.presentation.feature.detail
+
+import androidx.lifecycle.ViewModel
+
+class GameDetailViewModel() : ViewModel() {}
